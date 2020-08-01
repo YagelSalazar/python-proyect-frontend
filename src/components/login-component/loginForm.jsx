4c6c39072a4858be16c317f9c07538b9bd23dd1d
YagelSalazar <yagels.xd@gmail.com>
2020-07-31 19:11:36 -0500

    Login: Implemented login

---
+++ src/components/login-component/loginForm.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export const LoginForm = () => {
+    return (
+        <div className="container col-5 shadow" style={{ paddingBottom: "30px", marginTop: "40px",paddingTop: "15px" }}>
+            <div className="row d-flex justify-content-center" style={{ padding: "25px 0" }}>
+                <h5 className="text-muted" style={{ fontWeight: "bold" }}>Iniciar sesión en Trello</h5>
+            </div>
+            <div className="row d-flex justify-content-center">
+                <form style={{ width: "80%" }}>
+                    <div className="form-group">
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Introduzca el correo electónico" aria-describedby="emailHelp" />
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Introduzca la contraseña" />
+                    </div>
+                    <button type="submit" class="btn btn-success" style={{ width: "100%" }}>Iniciar sesión</button>
+                </form>
+            </div>
+            <div className="row d-flex justify-content-center" style={{ paddingTop: "25px" }}>
+                <p>o</p>
+            </div>
+            <div className="row d-flex justify-content-center" style={{ borderBottom: "solid 1px #7C7E83" }}>
+                <button type="button" class="btn btn-light shadow-sm" style={{ fontWeight: "bold", width: "80%", marginBottom: "20px" }}>Continuar con Google</button>
+                <button type="button" class="btn btn-light shadow-sm" style={{ fontWeight: "bold", width: "80%", marginBottom: "20px" }}>Continuar con Microsoft</button>
+                <button type="button" class="btn btn-light shadow-sm" style={{ fontWeight: "bold", width: "80%", marginBottom: "20px" }}>Continuar con Apple</button>
+                <p style={{ color: "#0952F9" }}>Iniciar sesión con SSO</p>
+            </div>
+            <div className="row d-flex justify-content-center" style={{ marginTop: "20px" }}>
+                <p style={{ color: "#0952F9" }}>¿No puede iniciar sesión?</p>
+                <p style={{ color: "#0952F9" }}>Regístrese para crear una cuenta</p>
+            </div>
+        </div>
+    )
+}
---
+++ src/views/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LoginForm } from '../components/login-component/loginForm'
+
+const Login = () => {
+    return(
+        <div className="container">
+            <LoginForm></LoginForm>
+        </div>
+    )
+}
+
+export default Login;
